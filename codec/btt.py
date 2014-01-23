6c407d09befae61e687709ea3b9c370fb54946b0
We Server <sfl@neusoft.edu.cn>
2014-01-23 08:41:06 +0800

    Using django native encoding functions.

--- codec/btt.py
+++
@@ -1,8 +0,0 @@
-from base64 import urlsafe_b64encode, urlsafe_b64decode
-from uuid import UUID
-
-def encode(uuid):
-    return urlsafe_b64encode(uuid.bytes.encode('base64')).rstrip('=')
-
-def decode(string):
-    return UUID(bytes=urlsafe_b64decode(string + '=='))
