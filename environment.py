545b1b8b4f0beb10bf58c6efc5f447c97b9dae0f
We Server <sfl@neusoft.edu.cn>
2014-03-09 08:57:20 +0800

    Adding open server domain.

--- environment.py
+++ environment.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+
 from ip import get_geo
+
+OPEN_SERVER = getattr(settings, 'OPEN_SERVER_DOMAIN')
 
 def get_environment(request):
     result = dict()
+    result.update({'open_server': OPEN_SERVER})
     result.update({'html5': 'Mozilla/5.0' in request.META.get('HTTP_USER_AGENT')})
     result.update({'intranet': get_geo(request) is not None})
 
