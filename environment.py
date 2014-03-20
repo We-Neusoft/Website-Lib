e047ccb8f52680a72f4531d0c5770554b81dd6b1
We Server <sfl@neusoft.edu.cn>
2014-03-20 08:37:23 +0800

    Fix NoneType UA error.

--- environment.py
+++ environment.py
@@ -7,7 +7,7 @@ OPEN_SERVER = getattr(settings, 'OPEN_SERVER_DOMAIN')
 def get_environment(request):
     result = dict()
     result.update({'open_server': OPEN_SERVER})
-    result.update({'html5': 'Mozilla/5.0' in request.META.get('HTTP_USER_AGENT')})
+    result.update({'html5': 'Mozilla/5.0' in request.META.get('HTTP_USER_AGENT', '')})
     result.update({'intranet': get_geo(request) is not None})
 
     return result
